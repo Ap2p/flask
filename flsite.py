821e0b8a316869886d6273f7b1fa5a22ab7544e7
Galstyan Arutyun <reged1t.galar@icloud.com>
2023-07-19 02:36:17 +0300

    added form autorized username added page404.html

--- flsite.py
+++ flsite.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, flash
+from flask import Flask, render_template, url_for, request, flash, redirect, session, abort
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'fqwesdfgqergfdgsfgadg54gs'
@@ -25,6 +25,8 @@ def about():
 
 @app.route('/profile/<username>')
 def profile(username):
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
     return f'Пользователь: {username}'
 
 
@@ -37,8 +39,21 @@ def contact():
             flash('Ошибка отправки', category='error')
     return render_template('contact.html', title='Обратная связь', menu=menu)
 
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] == 'ap2p' and request.form['psw'] == '123':
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+    return render_template('login.html', title='Авторизация', menu=menu)
+
 # with app.test_request_context():
 #     print(url_for('about'))
+
+@app.errorhandler(404)
+def pageNotFound(error):
+    return render_template('page404.html', title='Страница не найдена', menu=menu), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
