5d1e469e5869adcee49b6f57f669bc89be0b7a8b
Galstyan Arutyun <reged1t.galar@icloud.com>
2023-07-19 01:53:48 +0300

    init styles.css added form added /contact update base templates

--- flsite.py
+++ flsite.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
-menu = ['Установка', 'Первое приложение', 'Обратная связь']
+menu = [
+    {'name': 'Установка', 'url': 'install-flask'},
+    {'name': 'Первое приложение', 'url': 'first-app'},
+    {'name': 'Обратная связь', 'url': 'contact'},
+
+]
 
 
 @app.route('/')
@@ -15,6 +20,18 @@ def index():
 def about():
     print(url_for('about'))
     return render_template('about.html', title='О сайте', menu=menu)
+
+
+@app.route('/profile/<username>')
+def profile(username):
+    return f'Пользователь: {username}'
+
+
+@app.route('/contact', methods=['POST', 'GET'])
+def contact():
+    if request.method == 'POST':
+        print(request.form['username'])
+    return render_template('contact.html', title='Обратная связь', menu=menu)
 
 # with app.test_request_context():
 #     print(url_for('about'))
